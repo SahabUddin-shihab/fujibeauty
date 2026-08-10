d8ad133709bb423ab3601923e5b339e4e1b6815a
ShihabMahmod <shihabmahmod58@gmail.com>
2026-08-10 12:17:58 +0600

    develop utils package

---
+++ packages/utils/src/api-response.ts
@@ -0,0 +1,20 @@
+import type {Response} from 'express';
+
+
+export function sendSuccess<T>(
+  res: Response,
+  data: T,
+  message = "Success",
+  statusCode = 200
+): Response {
+  return res.status(statusCode).json({ success: true, message, data });
+}
+
+export function sendError(
+  res: Response,
+  message = "Something went wrong",
+  statusCode = 500,
+  errors?: unknown
+): Response {
+  return res.status(statusCode).json({ success: false, message, errors });
+}
