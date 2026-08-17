310a9af6aaee0e1a1025660d19e1e88fafec864e
ShihabMahmod <shihabmahmod58@gmail.com>
2026-08-17 10:09:04 +0600

    kafka config added in notification-service

---
+++ apps/notification-service/src/config/kafka.ts
@@ -0,0 +1,7 @@
+import { KafkaClient } from "@fujibeauty/kafka-client";
+import { env } from "./env";
+
+export const kafkaClient = new KafkaClient({
+  clientId: env.KAFKA_CLIENT_ID,
+  brokers: env.KAFKA_BROKERS.split(","),
+});
